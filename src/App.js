c3e8a634dd3899a1ba93bbc2bbbafff4aec674af
Crypter20 <80840908+Crypter20@users.noreply.github.com>
2022-01-08 23:21:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Autocomplete from "./Autocomplete";
 
 function App() {
+  const suggestions = [
+    "Javascript",
+    "react",
+    "NodeJs",
+    "ExpressJs",
+    "Firebase",
+    "MongoDB",
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Autocomplete Demo</h1>
+      <h2>Start typing and experience React autocomplete!</h2>
+      <Autocomplete suggestions={suggestions} />
     </div>
   );
 }
